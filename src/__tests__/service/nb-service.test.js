5fec9ba04487c35297516fb94110d6529b06b4ee
Paulo Rogério Sales Santos <paulosales@gmail.com>
2020-08-10 09:32:08 -0300

    Unit tests improvements

--- src/__tests__/service/nb-service.test.js
+++ src/__tests__/service/nb-service.test.js
@@ -1,29 +1,54 @@
 import { generateNb, formatNb } from '../../service/nb-service'
 
 describe('NbService', () => {
-  describe('#generate-nb', () => {
-    describe('when generate a new NB', () => {
-      beforeEach(() => {
-        jest
-          .spyOn(global.Math, 'random')
-          .mockReturnValueOnce(0.111)
-          .mockReturnValueOnce(0.666)
-          .mockReturnValueOnce(0.888)
-          .mockReturnValueOnce(0.777)
-          .mockReturnValueOnce(0.444)
-          .mockReturnValueOnce(0.888)
-          .mockReturnValueOnce(0.555)
-          .mockReturnValueOnce(0.222)
-          .mockReturnValueOnce(0.999)
-      })
+  const mockRandomSequence = (sequence) => {
+    if (typeof sequence === 'string') {
+      sequence = sequence.split('').map((item) => Number.parseInt(item))
+    }
+    sequence.forEach((item) => {
+      jest.spyOn(global.Math, 'random').mockReturnValueOnce(item / 9)
+    })
+  }
 
+  describe('#generate-nb', () => {
+    const nbs = [
+      '168748529',
+      '101297644',
+      '700057189',
+      '700095016',
+      '700095137',
+      '700084448',
+      '700097959',
+      '700098161',
+      '700098476',
+      '700100656',
+    ]
+    const nbsWithChecksum = [
+      '1687485299',
+      '1012976448',
+      '7000571897',
+      '7000950162',
+      '7000951371',
+      '7000844486',
+      '7000979594',
+      '7000981610',
+      '7000984768',
+      '7001006565',
+    ]
+
+    describe(`when generate the NBs ${nbs.join(', ')}`, () => {
       afterEach(() => {
         jest.spyOn(global.Math, 'random').mockRestore()
       })
 
-      it('should generate a correct checksum', () => {
-        const nb = generateNb()
-        expect(nb).toBe('1687485299')
+      it(`should generate the NBs with checksum ${nbsWithChecksum.join(
+        ', '
+      )}`, () => {
+        nbs.forEach((nb, i) => {
+          mockRandomSequence(nb)
+          const generatedNb = generateNb()
+          expect(generatedNb).toBe(nbsWithChecksum[i])
+        })
       })
     })
   })
